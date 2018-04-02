bb5a86a2b7ee1a4d968d91bae86eb917430ae454
piotrwanio <34741162+piotrwanio@users.noreply.github.com>
2018-04-03 00:58:10 +0200

    Modified Scores View and Activity (added fragments), also added Drawer as navigation side bar.

--- matchScore/app/src/main/java/com/example/piotr_wanio/matchscore/ScoresActivity.java
+++ matchScore/app/src/main/java/com/example/piotr_wanio/matchscore/ScoresActivity.java
@@ -1,12 +1,19 @@
 package com.example.piotr_wanio.matchscore;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
@@ -17,47 +24,75 @@ import com.example.piotr_wanio.matchscore.R;
 
 import java.util.ArrayList;
 
-public class ScoresActivity extends AppCompatActivity {
-    private SQLiteDatabase db;
-    private Cursor cursor;
+public class ScoresActivity extends Activity {
+
+    private String[] titles;
+    private ListView drawerList;
+    DrawerLayout drawerLayout;
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // Kod wykonywany po kliknięciu elementu w szufladzie nawigacyjnej
+            selectItem(position);
+        }
+    }
+
+    ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
-        ListView listResults = (ListView) findViewById(R.id.scoreslist);
+        titles = getResources().getStringArray(R.array.titles);
+        drawerList = (ListView) findViewById(R.id.drawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-
-        try {
-            SQLiteOpenHelper matchScoreDatabaseHelper = new MatchScoreDatabaseHelper(this);
-
-            String[] from = {"TEAM_A", "GOALS_A", "GOALS_B", "TEAM_B"};
-            int[] to = {R.id.listview_item_teamA, R.id.listview_item_goals_teamA,
-                    R.id.listview_item_goals_teamB, R.id.listview_item_teamB};
-
-            db = matchScoreDatabaseHelper.getReadableDatabase();
-            cursor = db.query("RESULT", new String[]{"_id", "TEAM_A", "GOALS_A", "GOALS_B", "TEAM_B"}, null, null, null, null, null);
-            CursorAdapter listAdapter = new SimpleCursorAdapter(this,
-                    R.layout.listview_activity,
-                    cursor,
-              //      new String[]{"TEAM_A", "TEAM_B"},
-               //     new int[]{android.R.id.text1},
-                    from,
-                    to,
-                    0);
-            listResults.setAdapter(listAdapter);
-        }catch (SQLiteException e){
-            Toast toast = Toast.makeText(this, "Baza danych jest niedostępna!", Toast.LENGTH_SHORT);
-
-            toast.show();
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_activated_1, titles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        // Wyświetlamy odpowiedni fragment
+        if (savedInstanceState == null) {
+            selectItem(0);
         }
+
     }
 
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        cursor.close();
-        db.close();
+    private void selectItem(int position) {
+        // Aktualizujemy główną zawartość aplikacji, podmieniając prezentowany fragment
+
+
+        Fragment fragment;
+        switch (position) {
+            case 1:
+                fragment = new ResultsFragment();
+                break;
+            case 2:
+                fragment = new ResultsFragment();
+                break;
+            default:
+                fragment = new ResultsFragment();
+        }
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, fragment, "visible_fragment");
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+        // Ustawiamy tytuł paska akcji
+        setActionBarTitle(position);
+        // Zamykamy szufladę nawigacyjną
+        drawerLayout.closeDrawer(drawerList);
     }
+
+    private void setActionBarTitle(int position) {
+        String title;
+        if (position == 0) {
+            title = getResources().getString(R.string.app_name);
+        } else {
+            title = titles[position];
+        }
+        getActionBar().setTitle(title);
+    }
+
 
 }
